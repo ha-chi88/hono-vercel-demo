f3afb5e19edcc707064c623b3bc63e1d0453dfee
jbtaku <jbta1086@gmail.com>
2024-05-15 16:46:11 +0900

    add

--- src/app/api/[route]/route.ts
+++ src/app/api/[route]/route.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { z } from "zod";
 
+export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { client } from "@/lib/hono";
 import React from "react";
 
+export const runtime = "edge";
+
 async function Page() {
   const res = await client.api.posts.$post({ json: { name: "TAKUMI" } });
   const result = await res.json();
