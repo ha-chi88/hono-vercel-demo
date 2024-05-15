12ca2d1c949831f1a280ed7accae55ff6c726819
jbtaku <jbta1086@gmail.com>
2024-05-15 16:15:21 +0900

    add

--- src/app/api/[route]/route.ts
+++ src/app/api/[route]/route.ts
@@ -1,14 +1,16 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
 
-app.get('/hello', (c) => {
+const route = app.get("/hello", (c) => {
   return c.json({
-    message: 'Hello Next.js!',
-  })
-})
+    message: "Hello Next.js!",
+  });
+});
 
-export const GET = handle(app)
+export type AppType = typeof route;
+
+export const GET = handle(app);
