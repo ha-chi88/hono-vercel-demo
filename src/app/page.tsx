b4712a7d92fab99ff0e26459f3b3e9172d6e0034
jbtaku <jbta1086@gmail.com>
2024-05-15 16:59:32 +0900

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { client } from "@/lib/hono";
 import React from "react";
 
-export const runtime = "edge";
-
 async function Page() {
   const res = await client.api.posts.$post({ json: { name: "TAKUMI" } });
   const result = await res.json();
